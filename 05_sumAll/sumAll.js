f5f528cfa21d355a1004bd71c141ee61bdd1589f
chrstinalin <44586776+chrstinalin@users.noreply.github.com>
2023-05-10 09:55:56 -0400

    js 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
 
+    if(!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) return "ERROR"
+
+    let sum=0
+    let min = x, max = y
+    if(y < x) min = y, max = x
+
+    for(let i=min; i<=max; i++){
+        sum+=i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
