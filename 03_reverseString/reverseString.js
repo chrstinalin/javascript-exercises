a276bb69d2c148b11c58b48fd246811a898963e5
chrstinalin <44586776+chrstinalin@users.noreply.github.com>
2023-05-10 09:23:24 -0400

    js 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let returnVal = ""
+    for(let i=string.length; i > 0; i--){
+        returnVal = returnVal.concat(string.charAt(i - 1))
+    }
+    return returnVal
 
 };
 
